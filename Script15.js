f59020aa6eacecd2915c292c057ce7f73dd870d5
Ericka Nasingao <98379705+erickanasingao@users.noreply.github.com>
2022-05-10 21:58:03 +0800

    Add files via upload

---
+++ Script15.js
@@ -0,0 +1,5 @@
+document.getElementById("personal").innerHTML="Personal Info";
+document.getElementById("contact").innerHTML="Contact Information";
+document.getElementById("acad").innerHTML="Academic Information";
+alert("Hello. This is a personal info \nClick OK");
+console.log("I love playing mobile legends:)");
